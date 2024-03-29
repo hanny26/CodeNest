3d96708ebd0e768539ab0f758660c6e227176b0a
hanny26 <hannyvyas109@gmail.com>
2024-03-29 19:36:25 +0530

    Auth jsx

--- client/src/Store/Auth.jsx
+++ client/src/Store/Auth.jsx
@@ -3,7 +3,7 @@ import { createContext } from "react";
 
 export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
-    return <AuthContext.Provider value={}>
+    return <AuthContext.Provider>
         {children}
         </AuthContext.Provider>;
 }
