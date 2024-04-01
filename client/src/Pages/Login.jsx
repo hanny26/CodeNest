38fb86f17d8208801c91dcce12683d50666c6bf5
hanny26 <hannyvyas109@gmail.com>
2024-04-01 20:11:06 +0530

    login , register

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,7 +1,7 @@
 // import { set } from "mongoose";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "./Store/Auth.jsx";
+import { useAuth } from '../Store/Auth';
 
 const URL = "http://localhost:5800/login";
 
