c956bbdb35c8f7ae9f6cf5378237b7d6c6797f40
hanny26 <hannyvyas109@gmail.com>
2024-04-27 18:41:37 +0530

    login changes

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -136,7 +136,7 @@ const Register = () => {
                     type="submit"
                     className="w-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
                   >
-                    Register Now
+                    login
                   </button>
                 </form>
               </div>
